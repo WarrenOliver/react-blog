589f2fda4c5a44d7db94b20635d777d1371c4cb8
WarrenOliver <warren@oliver.us>
2023-10-24 17:14:05 -0400

    added filtering feature

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,17 +1,20 @@
-const BlogList = ({blogs,title}) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
 
-    return (
-        <div className="blog-list">
-             <h1>{title}</h1>
-             {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <div>Written by: {blog.author}</div>
-                    <div>Blog Preview: {blog.body}</div>                    
-                </div>
-            ))}
+
+
+  return (
+    <div className="blog-list">
+      <h1>{title}</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <div>Written by: {blog.author}</div>
+          <div>Blog Preview: {blog.body}</div>
+          <button onClick={() = handleDelete(blog.id)}>Delete</button>
         </div>
-    );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,14 +4,31 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
-        {title: 'My New Website', body: 'lorem ipsum...', author: 'Mario', id: 1},
-        {title: 'Welcome Party!', body: 'lorem ipsum...', author: 'Luigi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Yoshi', id: 3}
+        { title: 'My New Website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
+        { title: 'Welcome Party!', body: 'lorem ipsum...', author: 'Luigi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 },
+        { title: 'Another Blog', body: 'lorem ipsum...', author: 'Peach', id: 4 },
+        { title: 'JavaScript Fundamentals', body: 'lorem ipsum...', author: 'Mario', id: 5 },
+        { title: 'React Hooks Tutorial', body: 'lorem ipsum...', author: 'Toad', id: 6 },
+        { title: 'CSS Styling Tricks', body: 'lorem ipsum...', author: 'Peach', id: 7 },
+        { title: 'Node.js Backend Development', body: 'lorem ipsum...', author: 'Bowser', id: 8 }
     ]);
-    const title = "boogi8e"
+
+    const handleDelete = (id) => {
+        
+    }
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" />
+            <BlogList
+              blogs={blogs}
+              title="All Blogs"
+              handleDelete={handleDelete}
+            />
+            
+  
+
+            
         </div>
         
     );
