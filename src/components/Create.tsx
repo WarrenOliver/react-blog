426a11e06aa9f11bfe91350d2ee7c02cefa1907f
WarrenOliver <warren@oliver.us>
2023-11-16 16:26:37 -0500

    added events and user-input-tracking to create forms

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
+    const [author, setAuthor] = useState("mario")
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                {/* Title field */}
+                <label>Blog Title:</label>
+                <input
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+
+                {/* Body field */}
+                <label>Blog Body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+
+                {/* Author field */}
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="luigi">luigi</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
+            <br /><br /><br />
+            <h1>{title}</h1>
+            <br />
+            <p>{body}</p>
+            <br />
+            <p>Author: {author}</p>
+
+
         </div>
     );
 }
