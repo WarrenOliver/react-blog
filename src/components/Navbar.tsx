8332ddcb10434671fb4ab1a46d7ebf74b59a0103
WarrenOliver <warren@oliver.us>
2023-11-29 16:55:52 -0500

    fixed navbar button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <a href="/"><h1>Warren's Blog</h1></a>
+            <a href="/react-blog"><h1>Warren's Blog</h1></a>
             <div className="links">
                 <Link to="/react-blog">Home</Link>
                 <Link to="/create">New Blog</Link>
